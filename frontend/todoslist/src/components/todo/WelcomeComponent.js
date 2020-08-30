0a618a8d8a9840cd59535425ba50e75b80ade2ff
vancouver29 <vancouver2990@gmail.com>
2020-08-30 19:52:52 +0200

    34- Connecting React with Restful API- 4 - Adding Axios and Creating Hel

--- frontend/todoslist/src/components/todo/WelcomeComponent.js
+++ frontend/todoslist/src/components/todo/WelcomeComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import HelloWorldService from "../../api/todo/HelloWorldService";
 
 export default class WelcomeComponent extends Component {
   constructor(props) {
@@ -30,6 +31,9 @@ export default class WelcomeComponent extends Component {
   }
 
   retrieveWelcomeMessage() {
-    console.log("retrieve clicked");
+    HelloWorldService.executeHelloWorldService().then((response) =>
+      console.log(response)
+    );
+    //.catch()
   }
 }
