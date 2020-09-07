a3b25ef1a29755385c7b0dc1416ae80cb86e3a39
vancouver29 <vancouver2990@gmail.com>
2020-09-07 17:33:25 +0200

    82-50- Implementing Form Validation with Formik

--- frontend/todoslist/src/components/todo/TodoComponent.js
+++ frontend/todoslist/src/components/todo/TodoComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import moment from "moment";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export default class TodoComponent extends Component {
   constructor(props) {
@@ -9,10 +9,26 @@ export default class TodoComponent extends Component {
     this.state = {
       id: 1,
       description: "Learn Forms Now",
-      targetDate: moment(new Date()).format("YYY-MM-DD"),
+      targetDate: moment(new Date()).format("YYYY-MM-DD"),
     };
 
     this.onSubmit = this.onSubmit.bind(this);
+    this.validate = this.validate.bind(this);
+  }
+
+  validate(values) {
+    let errors = {};
+    if (!values.description) {
+      errors.description = "Enter a Description";
+    } else if (values.description.length < 5) {
+      errors.description = "Enter at least 5 Characters in Description";
+    }
+
+    if (!moment(values.targetDate).isValid()) {
+      errors.targetDate = "Enter a valid Target Date";
+    }
+
+    return errors;
   }
 
   onSubmit(values) {
@@ -34,9 +50,22 @@ export default class TodoComponent extends Component {
               targetDate,
             }}
             onSubmit={this.onSubmit}
+            validateOnChange={false}
+            validateOnBlur={false}
+            validate={this.validate}
           >
             {(props) => (
               <Form>
+                <ErrorMessage
+                  name="description"
+                  component="div"
+                  className="alert alert-warning"
+                />
+                <ErrorMessage
+                  name="targetDate"
+                  component="div"
+                  className="alert alert-warning"
+                />
                 <fieldset className="form-group">
                   <label>Description</label>
                   <Field
