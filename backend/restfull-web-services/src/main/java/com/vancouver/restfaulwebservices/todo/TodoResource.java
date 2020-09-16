ee51fab4dc3b66984f61f1d79681f4239dfbde6e
vancouver29 <vancouver2990@gmail.com>
2020-09-16 10:07:18 +0200

    90-58 Setting up Spring Security

--- backend/restfull-web-services/src/main/java/com/vancouver/restfaulwebservices/todo/TodoResource.java
+++ backend/restfull-web-services/src/main/java/com/vancouver/restfaulwebservices/todo/TodoResource.java
@@ -2,9 +2,12 @@ package com.vancouver.restfaulwebservices.todo;
 
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,6 +22,11 @@ public class TodoResource {
         return todoService.findAll();
     }
 
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo getTodo(@PathVariable String username, @PathVariable long id) {
+        return todoService.findById(id);
+    }
+
     //DELETE /users/{username}/todos/{id}
     @DeleteMapping("/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
@@ -28,5 +36,28 @@ public class TodoResource {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
+    }
+
+    //Edit/Update a Todo
+    //PUT /users/{user_name}/todos/{todo_id}
+    @PutMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo){
+        Todo todoUpdated = todoService.save(todo);
+
+        return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+    }
+
+    //Create a new Todo
+    //POST /users/{user_name}/todos/
+    @PostMapping("/users/{username}/todos")
+    public ResponseEntity<Void> updateTodo(@PathVariable String username, @RequestBody Todo todo){
+        Todo createdTodo = todoService.save(todo);
+
+        //Location
+        //Get current resource url
+        //append {id}
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
